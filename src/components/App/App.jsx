13a5fed8ab629862edf11cdc3c43c76d6446b4b6
RuslanChekhuta <ruslan.gpt2025@gmail.com>
2026-03-12 20:46:08 +0100

    урок 1.3

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import MyName from "./MyName/MyName";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Привет, React!</h1>
+        <p>Это мой первый React-проект с Vite.</p>
+        <MyName />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/App/MyName/MyName.jsx
@@ -0,0 +1,5 @@
+import "./MyName.css";
+
+export default function MyName() {
+  return <h2>Меня зовут Алексей</h2>;
+}
