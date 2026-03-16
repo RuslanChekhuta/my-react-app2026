f88925ea348fc522107187d292fd732a164b706c
RuslanChekhuta <ruslan.gpt2025@gmail.com>
2026-03-16 23:01:46 +0100

    урок 1.6 поправки

--- src/components/PromoText/PromoText.jsx
+++ src/components/PromoText/PromoText.jsx
@@ -5,9 +5,6 @@ const PromoText = () => {
     <section className="promo-text">
       <h3>Функциональные компоненты</h3>
       <p>React позволяет собирать интерфейс из небольших независимых блоков</p>
-      <a href="https://react.dev/" target="_blank">
-        React Docs
-      </a>
     </section>
   );
 };
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,8 +6,13 @@ const AboutPage = () => {
     <section className="about-page">
       <h2>О странице About</h2>
       <p>Это отдельный page-компонент внутри папки pages</p>
+      <a href="https://react.dev/" target="_blank">
+        React Docs
+      </a>
       <img src={programmerImage} alt="Программист" />
+      <p>Первое изображение подключено из assets</p>
       <img src="/images/laptop.jpg" alt="Ноутбук" />
+      <p>Второе изображение подключено из public</p>
     </section>
   );
 };
